a7c2fdb50b434c6ba34ab3a283ec30d7d5577134
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-06-11 23:33:17 +0530

    added registration system with user table

--- flask_to_do_list/app/models.py
+++ flask_to_do_list/app/models.py
@@ -5,3 +5,16 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(20),default="pending")
     
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(
+        db.String(20),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(100),
+        nullable=False
+    )
--- flask_to_do_list/app/routes/auth.py
+++ flask_to_do_list/app/routes/auth.py
@@ -1,19 +1,38 @@
 from flask import redirect , url_for , flash , Blueprint , render_template , request , session
+from app import db
+from app.models import Users
 
 auth_bp = Blueprint('auth',__name__)
 
-USER_CREDENTIALS = {
-    'username':'admin',
-    'password':'1234'
-}
+
+@auth_bp.route('/register',methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username and password:
+            new_user=Users(username=username,password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("user registered successfully, Login now.","success")
+            return redirect(url_for('auth.login'))
+    return render_template("register.html")
+
 
 @auth_bp.route("/login", methods=["GET","POST"])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == USER_CREDENTIALS['username'] and password == USER_CREDENTIALS['password']:
-            session['user'] = username
+        print("Entered username:", username)
+        print("Entered password:", password)
+        users = Users.query.all()
+        for u in users:
+            print("DB:", u.username, u.password)
+            
+        user = Users.query.filter_by(username=username,password=password).first()
+        if user:
+            session['user'] = user.username
             flash('login successful', 'success')
             return redirect(url_for('tasks.view_tasks'))
         else:
@@ -24,6 +43,8 @@ def login():
         return redirect(url_for('tasks.view_tasks'))
 
     return render_template('login.html')
+
+
 
 @auth_bp.route('/logout')
 def logout():
