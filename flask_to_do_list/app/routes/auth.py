2a499b8617cfefa5a9ca773dcc29c95221dd6fef
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-06-13 11:28:20 +0530

    added a dynamic /hello user/  bar on top of task tab

--- flask_to_do_list/app/routes/auth.py
+++ flask_to_do_list/app/routes/auth.py
@@ -26,14 +26,14 @@ def login():
         password = request.form.get('password')
         user = Users.query.filter_by(username=username,password=password).first()
         if user:
-            session['user'] = user.id
+            session['user_id'] = user.id
             flash('login successful', 'success')
             return redirect(url_for('tasks.view_tasks'))
         else:
             flash('invalid username or password', 'danger')
 
     # If already logged in, redirect to tasks
-    if 'user' in session:
+    if 'user_id' in session:
         return redirect(url_for('tasks.view_tasks'))
 
     return render_template('login.html')
@@ -42,7 +42,8 @@ def login():
 
 @auth_bp.route('/logout')
 def logout():
-    session.pop('user',None)
+    session.pop('user_id',None)
     flash('logged out','info')
     return redirect(url_for('auth.login'))
-         
+
+
--- flask_to_do_list/app/routes/tasks.py
+++ flask_to_do_list/app/routes/tasks.py
@@ -1,27 +1,29 @@
 from flask import redirect , url_for , flash , Blueprint , render_template , request , session
 from app import db
-from app.models import Task
+from app.models import Task, Users
 
 
 tasks_bp = Blueprint('tasks',__name__)
 
 @tasks_bp.route('/')
 def view_tasks():
-    if 'user' not in session:
+    if 'user_id' not in session:
         return redirect(url_for('auth.login'))
-    user_id = session['user']
+    user_id = session['user_id']
     tasks = Task.query.filter_by(user_id=user_id).all()
-    return render_template('tasks.html',tasks=tasks)
+    if session.get('user_id'):
+                passeduser = Users.query.get(session['user_id'])
+    return render_template('tasks.html',tasks=tasks,passedusername=passeduser.username)
 
 
 @tasks_bp.route('/add',methods=["GET","POST"])
 def add_task():
-    if 'user' not in session:
+    if 'user_id' not in session:
         return redirect(url_for('auth.login'))
      
     title = request.form.get('title')
     if title:
-        new_task = Task(title=title, status='pending',user_id=session['user'])
+        new_task = Task(title=title, status='pending',user_id=session['user_id'])
         db.session.add(new_task)
         db.session.commit()
         flash('task added successfully','success')
@@ -46,11 +48,8 @@ def toggle_status(task_id):
 @tasks_bp.route('/clear',methods=["POST"])
 def clear_tasks():
     
-    print("LOGGED USER ID:", session.get('user'))
-    tasks = Task.query.filter_by(user_id=session['user']).all()
-    print("TASKS TO DELETE:", tasks)
-
-    Task.query.filter_by(user_id=session['user']).delete()
+    Task.query.filter_by(user_id=session['user_id']).delete()
     db.session.commit()
     flash("all tasks cleared","info")
     return redirect(url_for('tasks.view_tasks')  )
+
